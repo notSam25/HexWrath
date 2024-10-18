f4884e20c4897b76f2baba7973d823cb2717b5ec
Samuel Wiseman <>
2024-10-18 11:55:56 -0500

    styling

--- src/main.go
+++ src/main.go
@@ -10,14 +10,10 @@ import (
 )
 
 func main() {
-	// Create a new Gin router
 	router := gin.Default()
 
-	// Set up a template engine using html/template
-	router.SetFuncMap(template.FuncMap{
-		// Add any custom template functions here if needed
-	})
-	router.LoadHTMLGlob("src/templates/*") // Load templates from the "templates" directory
+	router.SetFuncMap(template.FuncMap{})
+	router.LoadHTMLGlob("src/templates/*")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "base.tmpl", gin.H{
